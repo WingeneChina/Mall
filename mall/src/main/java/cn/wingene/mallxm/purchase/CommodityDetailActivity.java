9c873b4b2313a866edc6ea53a19aaaa752231311
Wingene <linwenjunchina@163.com>
2017-08-07 22:19:17 +0800

    添加多屏适配解决方案，添加基础颜色，尺寸，样式

---
+++ mall/src/main/java/cn/wingene/mallxm/purchase/CommodityDetailActivity.java
@@ -0,0 +1,20 @@
+package cn.wingene.mallxm.purchase;
+
+import android.os.Bundle;
+
+import cn.wingene.mall.R;
+import cn.wingene.mallxf.ui.MyBaseActivity;
+
+/**
+ * Created by Wingene on 2017/8/6.
+ */
+
+public class CommodityDetailActivity extends MyBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commdity_detail);
+//        junze.android.R.dimen.
+    }
+}
