1e651db601a64bf42d865441cabe75116c7c5b25
yytyxj <1339419299@qq.com>
2017-08-12 10:14:30 +0800

    推荐界面搭建

--- mall/src/main/java/cn/wingene/mallxm/display/home/firstMenu/adapter/DaySpecialPriceAdapter.java
+++ mall/src/main/java/cn/wingene/mallxm/display/home/firstMenu/adapter/DaySpecialPriceAdapter.java
@@ -23,22 +23,6 @@ public class DaySpecialPriceAdapter extends RecyclerView.Adapter {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Log.e("", "click !");
-//                ObjectAnimator scale_X_objectAnimator = ObjectAnimator.ofFloat(v, "scaleX", 1f, 1.2f);
-//                scale_X_objectAnimator.setDuration(100);
-//                scale_X_objectAnimator.setInterpolator(new AccelerateInterpolator());
-//
-//                ObjectAnimator scale_Y_objectAnimator = ObjectAnimator.ofFloat(v, "scaleY", 1f, 1.2f);
-//                scale_Y_objectAnimator.setDuration(100);
-//                scale_Y_objectAnimator.setInterpolator(new AccelerateInterpolator());
-//
-//                ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(v, "translationZ", 0, 30);
-//                objectAnimator.setDuration(100);
-//                objectAnimator.setInterpolator(new AccelerateInterpolator());
-//                objectAnimator.start();
-//
-//                AnimatorSet animatorSet = new AnimatorSet();
-//                animatorSet.play(objectAnimator).with(scale_X_objectAnimator).with(scale_Y_objectAnimator);
             }
         });
         return new DaySpecialPrice(view);
