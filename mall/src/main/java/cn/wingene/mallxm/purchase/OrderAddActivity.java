971090322f4cd6bfc2bdd1ef5e12631d10be44fc
Wingene <linwenjunchina@163.com>
2017-08-15 22:37:12 +0800

    完成部分订单详情

---
+++ mall/src/main/java/cn/wingene/mallxm/purchase/OrderAddActivity.java
@@ -0,0 +1,17 @@
+package cn.wingene.mallxm.purchase;
+
+import android.os.Bundle;
+
+import cn.wingene.mallxf.ui.MyBaseActivity;
+
+/**
+ * Created by Wingene on 2017/8/14.
+ */
+
+public class OrderAddActivity extends MyBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
