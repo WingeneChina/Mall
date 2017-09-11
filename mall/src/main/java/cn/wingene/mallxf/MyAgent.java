9a95f3666a1107d80664909d6fde356c1643049a
Wingene <linwenjunchina@163.com>
2017-09-11 23:25:49 +0800

    解决商品规格选择的问题，物流信息显示的问题。

--- mall/src/main/java/cn/wingene/mallxf/MyAgent.java
+++ mall/src/main/java/cn/wingene/mallxf/MyAgent.java
@@ -179,6 +179,41 @@ public class MyAgent extends Agent {
         }
     }
 
+    public void startNav(){
+//        NaviPara para = new NaviPara();
+//        para.startPoint = pt1;
+//        para.startName= "从这里开始";
+//        para.endPoint  = pt2;
+//        para.endName   = "到这里结束";
+//
+//        try {
+//
+//            BaiduMapNavigation.openBaiduMapNavi(para, this);
+//
+//        } catch (BaiduMapAppNotSupportNaviException e) {
+//            e.printStackTrace();
+//            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//            builder.setMessage("您尚未安装百度地图app或app版本过低，点击确认安装？");
+//            builder.setTitle("提示");
+//            builder.setPositiveButton("确认", new OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    dialog.dismiss();
+//                    BaiduMapNavigation.GetLatestBaiduMapApp(MainActivity.this);
+//                }
+//            });
+//
+//            builder.setNegativeButton("取消", new OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    dialog.dismiss();
+//                }
+//            });
+//
+//            builder.create().show();
+//        }
+    }
+
     /**
      * 提示未安装百度地图app或app版本过低
      */
