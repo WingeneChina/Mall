6129757b0026af27355a96d95199f5cfd1f94bfc
Wingene <linwenjunchina@163.com>
2017-10-08 21:53:19 +0800

    增加是否驾培商品，是否显示购物车的控制。

--- mall/src/main/java/cn/wingene/mallxf/http/HttpConstant.java
+++ mall/src/main/java/cn/wingene/mallxf/http/HttpConstant.java
@@ -8,8 +8,8 @@ package cn.wingene.mallxf.http;
 
 public class HttpConstant {
     //    public static final String SERVER_ADDRESS = ;
-//    public static final String SERVER_ADDRESS = "http://api.52lime.cn/";
-    public static final String SERVER_ADDRESS = "http://guangheapp.usoft100.com/";
+    public static final String SERVER_ADDRESS = "http://api.52lime.cn/";
+//    public static final String SERVER_ADDRESS = "http://guangheapp.usoft100.com/";
 //    public static final String SERVER_ADDRESS = BuildConfig.DEBUG ? "http://guangheapp.usoft100.com/" :
 //            "http://api.52lime.cn/";
 
